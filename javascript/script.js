57267213077046013c69bf97ee65368c13f470a7
Win Lin Tun <www.iceface@gmail.com>
2023-11-27 15:10:58 +0630

    smooth scrolling

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,25 @@
 document.querySelector(".btn-mobile-nav").addEventListener("click", function () {
     document.querySelector(".header").classList.toggle("nav-open");
 })
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function(link){
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+        const herf = link.getAttribute("href");
+        if(herf === "#"){
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        }
+        if(herf !== "#" && herf.startsWith("#")) {
+            const selectEL = document.querySelector(herf);
+            selectEL.scrollIntoView({
+                behavior: "smooth",
+            })
+        }
+    });
+});
+
